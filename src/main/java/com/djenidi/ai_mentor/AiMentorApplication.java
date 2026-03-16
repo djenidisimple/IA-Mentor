d373a4c024af8a78cafb9137dd160db0a95c4a0d
djenidisimple <djenidigauss@gmail.com>
2026-03-16 04:04:27 +0300

    init project

---
+++ src/main/java/com/djenidi/ai_mentor/AiMentorApplication.java
@@ -0,0 +1,13 @@
+package com.djenidi.ai_mentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiMentorApplication.class, args);
+	}
+
+}
